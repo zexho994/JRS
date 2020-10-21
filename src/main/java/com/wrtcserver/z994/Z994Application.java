f464e3621968ccbb90a458082569288a44da1208
zexho <zouzhihao994@gmail.com>
2020-10-21 12:47:43 +0800

    init

---
+++ src/main/java/com/wrtcserver/z994/Z994Application.java
@@ -0,0 +1,13 @@
+package com.wrtcserver.z994;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z994Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Z994Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wrtcserver/z994/Z994ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrtcserver.z994;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z994ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
