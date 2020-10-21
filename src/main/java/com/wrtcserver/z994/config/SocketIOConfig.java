cf4fc93532252ec5a32a4797102bfd294299035a
zexho <zouzhihao994@gmail.com>
2020-10-21 13:10:31 +0800

    feat:add SocketIOConfig

---
+++ src/main/java/com/wrtcserver/z994/config/SocketIOConfig.java
@@ -0,0 +1,58 @@
+package com.wrtcserver.z994.config;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketConfig;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Zexho
+ * @date 2020/10/20 5:49 下午
+ */
+@org.springframework.context.annotation.Configuration
+public class SocketIOConfig {
+    @Value("${socketio.host}")
+    private String host;
+
+    @Value("${socketio.port}")
+    private Integer port;
+
+    @Value("${socketio.bossCount}")
+    private int bossCount;
+
+    @Value("${socketio.workCount}")
+    private int workCount;
+
+    @Value("${socketio.allowCustomRequests}")
+    private boolean allowCustomRequests;
+
+    @Value("${socketio.upgradeTimeout}")
+    private int upgradeTimeout;
+
+    @Value("${socketio.pingTimeout}")
+    private int pingTimeout;
+
+    @Value("${socketio.pingInterval}")
+    private int pingInterval;
+
+    @Bean
+    public SocketIOServer SocketIOServer() {
+        SocketConfig socketConfig = new SocketConfig();
+        socketConfig.setTcpNoDelay(true);
+        socketConfig.setSoLinger(0);
+
+        Configuration config = new Configuration();
+        config.setSocketConfig(socketConfig);
+        config.setHostname(host);
+        config.setPort(port);
+        config.setBossThreads(bossCount);
+        config.setWorkerThreads(workCount);
+        config.setAllowCustomRequests(allowCustomRequests);
+        config.setUpgradeTimeout(upgradeTimeout);
+        config.setPingTimeout(pingTimeout);
+        config.setPingInterval(pingInterval);
+
+        return new SocketIOServer(config);
+    }
+}
