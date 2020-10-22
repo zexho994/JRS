801fa535644e3c6bc02dd0c9b36789751279e1bf
zexho <zouzhihao994@gmail.com>
2020-10-22 15:24:40 +0800

    创建socket服务接口类

---
+++ src/main/java/com/wrtcserver/z994/socket/ISocketService.java
@@ -0,0 +1,15 @@
+package com.wrtcserver.z994.socket;
+
+public interface ISocketService {
+
+    /**
+     * 启动 socket io
+     */
+    void start();
+
+    /**
+     * 停止 socket io
+     */
+    void stop();
+
+}
